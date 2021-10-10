fd2d0cde744d8f4edc3dad94390fd3244a8d7cf0
Mike-Like11 <57439449+Mike-Like11@users.noreply.github.com>
2021-10-10 21:51:45 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/studentio/StudentioApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studentio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudentioApplication
+
+fun main(args: Array<String>) {
+	runApplication<StudentioApplication>(*args)
+}
